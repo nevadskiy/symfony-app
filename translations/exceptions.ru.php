03aac6b2eef6a40fa7530440a9478becb4f2c40c
Vitalii Kalaida <nevadskiy@gmail.com>
2019-07-01 22:42:29 +0300

    Add translations

---
+++ translations/exceptions.ru.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Invalid token' => 'Недействительный токен',
+    'User already exists' => 'Пользователь уже существует',
+    'Social network is already attached.' => 'Данная социальная сеть уже подключена',
+    'User is already confirmed' => 'Пользователь уже подтвержден',
+    'User is not active' => 'Пользователь неактивный',
+    'Email is not specified' => 'Email адрес не предоставлен',
+    'Reset password is already requested' => 'Сброс пароля уже запрошен',
+    'Resetting password has not been requested' => 'Сброс пароля не был запрошен',
+    'Reset password token is expired' => 'Токен сброса пароля устарел',
+    'Role is already set' => 'Роль уже установлена',
+];
