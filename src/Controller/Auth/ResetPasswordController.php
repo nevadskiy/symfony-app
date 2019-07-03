8c13fcae20b54535bb13627717788a65be137261
Vitalii Kalaida <nevadskiy@gmail.com>
2019-07-03 21:37:48 +0300

    Add light read-model repository

--- src/Controller/Auth/ResetPasswordController.php
+++ src/Controller/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Auth;
 
 use App\Model\User\UseCase\ResetPassword;
+use App\ReadModel\User\UserFetcher;
 use DomainException;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -59,11 +60,22 @@ class ResetPasswordController extends AbstractController
      * @param string $token
      * @param Request $request
      * @param ResetPassword\Reset\Handler $handler
+     * @param UserFetcher $users
      * @return Response
      * @throws \Exception
      */
-    public function reset(string $token, Request $request, ResetPassword\Reset\Handler $handler): Response
+    public function reset(
+        string $token,
+        Request $request,
+        ResetPassword\Reset\Handler $handler,
+        UserFetcher $users
+    ): Response
     {
+        if ($users->existsByResetPasswordToken($token)) {
+            $this->addFlash('error', 'Incorrect or already confirmed token.');
+            return $this->redirectToRoute('home');
+        }
+
         $command = new ResetPassword\Reset\Command($token);
 
         $form = $this->createForm(ResetPassword\Reset\Form::class, $command);
