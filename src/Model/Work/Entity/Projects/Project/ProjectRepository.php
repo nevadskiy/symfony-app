978ba6b08696432f3cd2b76e94939eedc8e3748a
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-22 22:50:22 +0300

    Add project entity with crud use cases

---
+++ src/Model/Work/Entity/Projects/Project/ProjectRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Work\Entity\Projects\Project;
+
+use App\Model\EntityNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProjectRepository
+{
+    private $repo;
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->repo = $em->getRepository(Project::class);
+        $this->em = $em;
+    }
+
+    public function get(Id $id): Project
+    {
+        $project = $this->find($id);
+
+        if (! $project) {
+            throw new EntityNotFoundException('Project is not found.');
+        }
+
+        return $project;
+    }
+
+    public function find(Id $id): ?Project
+    {
+        return $this->repo->find($id->getValue());
+    }
+
+    public function add(Project $project): void
+    {
+        $this->em->persist($project);
+    }
+
+    public function remove(Project $project): void
+    {
+        $this->em->remove($project);
+    }
+}
